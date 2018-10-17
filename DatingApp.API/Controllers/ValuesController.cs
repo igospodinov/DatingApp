302c8b365371cf5d5b584c05f40982ab6a29e751
Ivan <ivan.gospodinov@bgosoftware.com>
2018-10-17 08:56:06 +0300

    Client site register/login

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace Dating.API.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
